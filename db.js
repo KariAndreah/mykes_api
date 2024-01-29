b4802b56a8c4d3dd924ed3ee05e69ce92c4d823d
Kari Burt <kandreahburt@gmail.com>
2024-01-29 01:35:55 -0500

    changing vars

--- db.js
+++ db.js
@@ -11,11 +11,11 @@ const { Pool, Client } = pg;
 // });
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
