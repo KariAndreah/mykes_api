6a83de73dd2b1fb379294d45ea1af1ca802e9317
Kari Burt <kandreahburt@gmail.com>
2024-01-31 02:50:53 -0500

    trying to fix cors issue

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import boroughDayFreeRouter from "./app/mics/routes/borough-day-free-route.js";
 import dayRouter from "./app/mics/routes/day-route.js";
 import dayFreeRouter from "./app/mics/routes/day-free-route.js";
 import individualRouter from "./app/mics/routes/individual-route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 9999;
@@ -20,13 +21,15 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
 app.use((_, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
   next();
 });
