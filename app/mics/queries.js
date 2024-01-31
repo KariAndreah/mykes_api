444ce4bd8b838ac83b24018d16367678d38ebe77
Kari Burt <kandreahburt@gmail.com>
2024-01-31 01:35:09 -0500

    fixing orders

--- app/mics/queries.js
+++ app/mics/queries.js
@@ -1,33 +1,34 @@
 const innerJoin =
-  "INNER JOIN mic_address ON mics.address_id = mic_address.address_id INNER JOIN mic_cost ON mics.cost_id = mic_cost.cost_id ";
+  "INNER JOIN mic_address ON mics.address_id = mic_address.address_id INNER JOIN mic_cost ON mics.cost_id = mic_cost.cost_id ORDER BY borough asc, start_time asc ";
 
 const getAllMicListing =
-  "SELECT * FROM mics INNER JOIN mic_address ON mics.address_id = mic_address.address_id INNER JOIN mic_cost ON mics.cost_id = mic_cost.cost_id";
+  "SELECT * FROM mics INNER JOIN mic_address ON mics.address_id = mic_address.address_id INNER JOIN mic_cost ON mics.cost_id = mic_cost.cost_id ORDER BY borough asc, start_time asc";
 
 const getAllFreeMicListing =
-  "SELECT * FROM mics INNER JOIN mic_address ON mics.address_id = mic_address.address_id WHERE cost_id=1";
+  "SELECT * FROM mics INNER JOIN mic_address ON mics.address_id = mic_address.address_id WHERE cost_id=1 ORDER BY borough asc, start_time asc";
 
-const getMicListing = "SELECT * FROM mics WHERE";
+const getMicListing = "SELECT * FROM mics ORDER BY borough asc, start_time asc";
 
-const getMicById = "SELECT * FROM mics WHERE id = $1";
+const getMicById =
+  "SELECT * FROM mics WHERE id = $1 ORDER BY borough asc, start_time asc";
 
-const getMicListingByBorough = `SELECT * FROM mics ${innerJoin} WHERE Borough = ANY($1)`;
+const getMicListingByBorough = `SELECT * FROM mics ${innerJoin} WHERE Borough = ANY($1) ORDER BY borough asc, start_time asc`;
 
 const getMicListingByBoroughFree =
-  "SELECT * FROM mics WHERE Borough = ANY($1) AND cost_id =1";
+  "SELECT * FROM mics WHERE Borough = ANY($1) AND cost_id =1 ORDER BY borough asc, start_time asc";
 
-const getMicListingByDay = `SELECT * FROM mics ${innerJoin} WHERE Day = $1`;
+const getMicListingByDay = `SELECT * FROM mics ${innerJoin} WHERE Day = $1 ORDER BY borough asc, start_time asc`;
 
-const getMicListingByDayFree = `SELECT * FROM mics ${innerJoin} WHERE Day = $1`;
+const getMicListingByDayFree = `SELECT * FROM mics ${innerJoin} WHERE Day = $1 ORDER BY borough asc, start_time asc`;
 
 // const getMicListingByBoroughDay =
 //   "SELECT * FROM mics JOIN mic_address ON mics.address_id = mic_address.address_id JOIN mic_cost ON mics.cost_id = mic_cost.cost_id WHERE borough = ($1) AND day = $2";
 
 const getMicListingByBoroughDay =
-  "SELECT * FROM mics JOIN mic_address ON mics.address_id = mic_address.address_id JOIN mic_cost ON mics.cost_id = mic_cost.cost_id WHERE borough = ANY($1) AND day = $2";
+  "SELECT * FROM mics JOIN mic_address ON mics.address_id = mic_address.address_id JOIN mic_cost ON mics.cost_id = mic_cost.cost_id WHERE borough = ANY($1) AND day = $2 ORDER BY borough asc, start_time asc";
 
 const getMicListingByBoroughDayFree =
-  "SELECT * FROM mics JOIN mic_address ON mics.address_id = mic_address.address_id WHERE borough = ANY($1) AND day = $2 AND cost_id=1";
+  "SELECT * FROM mics JOIN mic_address ON mics.address_id = mic_address.address_id WHERE borough = ANY($1) AND day = $2 AND cost_id=1 ORDER BY borough asc, start_time asc";
 
 export {
   getMicListing,
--- index.js
+++ index.js
@@ -22,11 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use((_, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
