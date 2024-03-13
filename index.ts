ab253e2879799f8429188316efbbda0925ccaa16
Kari Burt <kandreahburt@gmail.com>
2024-03-13 01:11:06 -0400

    more cleanup

--- index.ts
+++ index.ts
@@ -1,18 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
-// import sunday_mic from "./routes/mics.js";
-// import sundayMics from "./routes/mics.js";
-import allFreeRouter from "./app/mics/routes/all-free-route.js";
-import allRouter from "./app/mics/routes/all-route.js";
-import boroughRouter from "./app/mics/routes/borough-route.js";
-import boroughFreeRouter from "./app/mics/routes/borough-free-route.js";
-import boroughDayRouter from "./app/mics/routes/borough-day-route.js";
-import boroughDayFreeRouter from "./app/mics/routes/borough-day-free-route.js";
-import boroughDayTimeRouter from "./app/mics/routes/borough-day-time-route.js";
-import boroughDayFreeTimeRouter from "./app/mics/routes/borough-day-time-free-route.js";
-import dayRouter from "./app/mics/routes/day-route.js";
-import dayFreeRouter from "./app/mics/routes/day-free-route.js";
-import individualRouter from "./app/mics/routes/individual-route.js";
 import cors from "cors";
 import { api, mic } from "./src/routes/routes.js";
 
@@ -43,22 +29,10 @@ app.use((_, res, next) => {
   );
   next();
 });
-// app.use('/mics', sundayMics)
-// app.use("/", sunday_mic);
+
 app.get("/", (req, res) => {
   res.send("Hello from Homepage");
 });
-// app.use("/mics/all", allRouter);
-// app.use("/mics/free", allFreeRouter);
-// app.use("/mics/borough", boroughRouter);
-// app.use("/mics/freeBorough", boroughFreeRouter);
-// app.use("/mics/day", dayRouter);
-// app.use("/mics/freeDay", dayFreeRouter);
-// app.use("/mics/boroughDay", boroughDayRouter);
-// app.use("/mics/freeBoroughDay", boroughDayFreeRouter);
-// app.use("/mics/boroughDayTime", boroughDayTimeRouter);
-// app.use("/mics/freeBoroughDayTime", boroughDayFreeTimeRouter);
-// app.use("/mic", individualRouter);
 
 app.use(api, mic);
 
