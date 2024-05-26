99083f17a3be2bfb39777a2abff95550eacde083
Kari Burt <kandreahburt@gmail.com>
2024-05-26 19:56:54 -0400

    cleaning up code

--- src/controllers/mics.controller.ts
+++ src/controllers/mics.controller.ts
@@ -1,5 +1,4 @@
 import express from "express";
-
 import {
   getMics,
   getMic,
@@ -9,12 +8,10 @@ import {
 
 const router = express.Router();
 
-const micsController = router.get("/mics", async (req, res, next) => {
-  let pageNo: any = req.query.pageNo || 1;
-  let pageSize: any = req.query.pageSize || 10;
-
-  let borough: any = req.query.borough;
-  var boroughArray = borough?.split(",");
+// Parameter handling function
+const parseParams = (req) => {
+  let borough = req.query.borough;
+  let boroughArray = borough ? borough.split(",") : [];
 
   if (borough === "all") {
     boroughArray = [
@@ -25,11 +22,9 @@ const micsController = router.get("/mics", async (req, res, next) => {
       "brooklyn",
     ];
   }
-  // console.log("this is what is going to params", borough);
-  let day: any = req.query.day;
-  let dayArray = day?.split(",");
 
-  console.log("HELLO KARI from mics ");
+  let day = req.query.day;
+  let dayArray = day ? day.split(",") : [];
 
   if (day === "all") {
     dayArray = [
@@ -43,133 +38,7 @@ const micsController = router.get("/mics", async (req, res, next) => {
     ];
   }
 
-  let params = {
-    day: dayArray,
-    borough: boroughArray,
-    limit: Number(req.query.limit) || 10,
-    offset: Number(req.query.offset) || 0,
-    start_time: req.query.start_time,
-    cost: req.query.free || "false",
-  };
-
-  // console.log("This is the start time: ", params.start_time);
-
-  // console.log("This is the borough", typeof borough);
-  // console.log("This is the Cost: ", params.cost);
-  try {
-    const { mics, count } = await getMics(params);
-
-    const pageCount = Math.ceil(mics.length / 10);
-
-    if (!pageNo) {
-      pageNo = 1;
-    }
-    if (pageNo > pageCount) {
-      pageNo = pageCount;
-    }
-
-    res.status(200).json({
-      totalMics: count,
-      offset: params.offset,
-      limit: params.limit,
-      mics: mics,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-const micController = router.get("/mic", async (req, res, next) => {
-  let page: any = req.query.page;
-  let id: any = req.query.id;
-
-  // console.log("This is the Page Number: ", page);
-  try {
-    const mics = await getMic(id);
-
-    // const pageCount = Math.ceil(mics.length / 10);
-
-    // if (!page) {
-    //   page = 1;
-    // }
-    // if (page > pageCount) {
-    //   page = pageCount;
-    // }
-
-    res.status(200).json({
-      currentPage: page,
-      totalPages: 1,
-      totalMics: 1,
-      mic: mics,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-const micHostController = router.get("/micHost", async (req, res, next) => {
-  let page: any = req.query.page;
-  let id: any = req.query.id;
-
-  // console.log("This is the Page Number: ", page);
-  try {
-    const mics = await getMicHost(id);
-
-    // const pageCount = Math.ceil(mics.length / 10);
-
-    // if (!page) {
-    //   page = 1;
-    // }
-    // if (page > pageCount) {
-    //   page = pageCount;
-    // }
-
-    res.status(200).json({
-      currentPage: page,
-      totalPages: 1,
-      totalMics: 1,
-      mic: mics,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-const micTimesController = router.get("/micTimes", async (req, res, next) => {
-  let pageNo: any = req.query.pageNo || 1;
-  let pageSize: any = req.query.pageSize || 10;
-
-  let borough: any = req?.query.borough;
-  var boroughArray = borough.split(",");
-
-  if (borough === "all") {
-    boroughArray = [
-      "manhattan",
-      "queens",
-      "staten-island",
-      "bronx",
-      "brooklyn",
-    ];
-  }
-  console.log("this is what is going to params", borough);
-  let day: any = req.query.day;
-  let dayArray = day?.split(",");
-
-  if (day === "all") {
-    dayArray = [
-      "sunday",
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-    ];
-  }
-
-  console.log(" This is coming from url", req.query["start-time"]);
-  console.log("is this printing");
-  let params = {
+  return {
     day: dayArray,
     borough: boroughArray,
     limit: Number(req.query.limit) || 10,
@@ -177,34 +46,61 @@ const micTimesController = router.get("/micTimes", async (req, res, next) => {
     start_time: req.query["start-time"] || "00:00:00",
     cost: req.query.free || "false",
   };
+};
 
-  console.log("This is the start time KARI BURT: ", params.start_time);
-
-  console.log("This is the borough", typeof borough);
-  // console.log("This is the Cost: ", params.cost);
+// Route handlers
+const micsController = router.get("/mics", async (req, res, next) => {
   try {
-    const { mics, count } = await getMicTimes(params);
-
-    const pageCount = Math.ceil(mics.length / 10);
-
-    if (!pageNo) {
-      pageNo = 1;
-    }
-    if (pageNo > pageCount) {
-      pageNo = pageCount;
-    }
-
-    res.status(200).json({
-      totalMics: count,
-      offset: params.offset,
-      limit: params.limit,
-      mics: mics,
-    });
+    const params = parseParams(req);
+    const { mics, count } = await getMics(params);
+    res
+      .status(200)
+      .json({
+        totalMics: count,
+        offset: params.offset,
+        limit: params.limit,
+        mics,
+      });
   } catch (error) {
     next(error);
   }
 });
 
-// console.log("Controller is working -----", micController);
+const micController = router.get("/mic", async (req, res, next) => {
+  try {
+    const id = req.query.id;
+    const mic = await getMic(id);
+    res.status(200).json({ mic });
+  } catch (error) {
+    next(error);
+  }
+});
+
+const micHostController = router.get("/micHost", async (req, res, next) => {
+  try {
+    const id = req.query.id;
+    const mic = await getMicHost(id);
+    res.status(200).json({ mic });
+  } catch (error) {
+    next(error);
+  }
+});
+
+const micTimesController = router.get("/micTimes", async (req, res, next) => {
+  try {
+    const params = parseParams(req);
+    const { mics, count } = await getMicTimes(params);
+    res
+      .status(200)
+      .json({
+        totalMics: count,
+        offset: params.offset,
+        limit: params.limit,
+        mics,
+      });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { micController, micsController, micTimesController, micHostController };
--- src/services/mics.service.ts
+++ src/services/mics.service.ts
@@ -3,98 +3,30 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getMics = async (params: any) => {
-  let mic_cost: any;
+  const mic_cost = params.cost === "true" ? 1 : undefined;
 
-  // console.log(params);
+  const passingBorough = params.borough.includes("")
+    ? ["manhattan", "queens", "staten-island", "bronx", "brooklyn"]
+    : params.borough;
 
-  if (params.cost === "true") {
-    mic_cost = 1;
-    // console.log("Is this working???");
-  }
-
-  // console.log(
-  //   "This is what was passed",
-  //   params.cost,
-  //   "This is the Cost id",
-  //   mic_cost
-  // );
-  let passingBorough;
-  if (params.borough.includes("")) {
-    passingBorough = [
-      "manhattan",
-      "queens",
-      "staten-island",
-      "bronx",
-      "brooklyn",
-    ];
-    //   console.log("This is the borough", params.borough);
-    //   if (params.borough.includes("All")) {
-    //     passingBorough = [
-    //       "Manhattan",
-    //       "Queens",
-    //       "Staten-Island",
-    //       "Bronx",
-    //       "Brooklyn",
-    //     ];
-    //   }
-  } else {
-    // console.log("else is working");
-    passingBorough = params.borough;
-  }
-
-  console.log(
-    "This is the params",
-    params.borough,
-    "This is the passingBorough"
-    // passingBorough
-  );
-
-  let passingDay;
-  if (params.day.includes("")) {
-    passingDay = [
-      "sunday",
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-    ];
-    //   console.log("This is the day", params.day);
-    //   if (params.day.includes("All")) {
-    //     passingDay = [
-    //       "Sunday",
-    //       "Monday",
-    //       "Tuesday",
-    //       "Wednesday",
-    //       "Thursday",
-    //       "Friday",
-    //       "Saturday",
-    //     ];
-    //   }
-  } else {
-    console.log("else is working");
-    passingDay = params.day;
-  }
-
-  console.log(
-    "This is the params",
-    params.day,
-    "This is the passingDay"
-    // passingDay
-  );
+  const passingDay = params.day.includes("")
+    ? [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+      ]
+    : params.day;
 
   const [mics, count] = await prisma.$transaction([
     prisma.mics.findMany({
-      include: {
-        mic_address: true,
-        mic_cost: true,
-        mic_occurrence: true,
-      },
+      include: { mic_address: true, mic_cost: true, mic_occurrence: true },
       where: {
         day: { in: passingDay },
         borough: { in: passingBorough },
-        // start_time: params.start_time,
         cost_id: mic_cost,
       },
       orderBy: { id: "asc" },
@@ -105,7 +37,6 @@ const getMics = async (params: any) => {
       where: {
         day: { in: passingDay },
         borough: { in: passingBorough },
-        // start_time: params.start_time,
         cost_id: mic_cost,
       },
     }),
@@ -115,180 +46,47 @@ const getMics = async (params: any) => {
 };
 
 const getMic = async (id?: number) => {
-  const mics = await prisma.mics.findUnique({
+  return await prisma.mics.findUnique({
     where: { id: id },
     include: {
       mic_address: true,
       mic_cost: true,
       mic_occurrence: true,
       signup_instructions: true,
-      host_mics: {
-        include: {
-          mic_host: true,
-        },
-      },
     },
   });
-
-  return mics;
 };
 
-// Test to figure out many-to-many host_mics
 const getMicHost = async (id?: number) => {
-  const mics = await prisma.mics.findUnique({
-    where: { id: id },
-    include: {
-      mic_address: true,
-      mic_cost: true,
-      mic_occurrence: true,
-      signup_instructions: true,
-      host_mics: {
-        include: {
-          mic_host: true,
-        },
-      },
-    },
-  });
-
-  return mics;
+  return await getMic(id); // Assuming same functionality as getMic
 };
 
 const getMicTimes = async (params: any) => {
-  let mic_cost: any;
+  const mic_cost = params.cost === "true" ? 1 : undefined;
 
-  console.log(params);
+  const passingBorough = params.borough.includes("")
+    ? ["manhattan", "queens", "staten-island", "bronx", "brooklyn"]
+    : params.borough;
 
-  if (params.cost === "true") {
-    mic_cost = 1;
-    console.log("Is this working???");
-  }
+  const passingDay = params.day.includes("")
+    ? [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+      ]
+    : params.day;
 
-  // console.log(
-  //   "This is what was passed",
-  //   params.cost,
-  //   "This is the Cost id",
-  //   mic_cost
-  // );
-  let passingBorough;
-  if (params.borough.includes("")) {
-    passingBorough = [
-      "manhattan",
-      "queens",
-      "staten-island",
-      "bronx",
-      "brooklyn",
-    ];
-    //   console.log("This is the borough", params.borough);
-    //   if (params.borough.includes("All")) {
-    //     passingBorough = [
-    //       "Manhattan",
-    //       "Queens",
-    //       "Staten-Island",
-    //       "Bronx",
-    //       "Brooklyn",
-    //     ];
-    //   }
-  } else {
-    console.log("else is working");
-    passingBorough = params.borough;
-  }
-
-  console.log(
-    "This is the params",
-    params.borough,
-    "This is the passingBorough"
-    // passingBorough
-  );
-
-  let passingDay;
-  if (params.day.includes("")) {
-    passingDay = [
-      "sunday",
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday",
-    ];
-    //   console.log("This is the day", params.day);
-    //   if (params.day.includes("All")) {
-    //     passingDay = [
-    //       "Sunday",
-    //       "Monday",
-    //       "Tuesday",
-    //       "Wednesday",
-    //       "Thursday",
-    //       "Friday",
-    //       "Saturday",
-    //     ];
-    //   }
-  } else {
-    console.log("else is working");
-    passingDay = params.day;
-  }
-
-  console.log(
-    "This is the params",
-    params.day,
-    "This is the passingDay"
-    // passingDay
-  );
-
-  let passingTime;
-  const timesAllowed = [
-    "00:00:00",
-    "01:00:00",
-    "02:00:00",
-    "03:00:00",
-    "04:00:00",
-    "05:00:00",
-    "06:00:00",
-    "07:00:00",
-    "08:00:00",
-    "09:00:00",
-    "10:00:00",
-    "11:00:00",
-    "12:00:00",
-    "13:00:00",
-    "14:00:00",
-    "15:00:00",
-    "16:00:00",
-    "17:00:00",
-    "18:00:00",
-    "19:00:00",
-    "20:00:00",
-    "21:00:00",
-    "22:00:00",
-    "23:00:00",
-  ];
-  if (params.start_time.includes("00:00")) {
-    passingTime = `1970-01-01T${params.start_time}.000Z`;
-    //   console.log("This is the day", params.day);
-    //   if (params.day.includes("All")) {
-    //     passingDay = [
-    //       "Sunday",
-    //       "Monday",
-    //       "Tuesday",
-    //       "Wednesday",
-    //       "Thursday",
-    //       "Friday",
-    //       "Saturday",
-    //     ];
-    //   }
-  } else {
-    console.log("else is working");
-    passingTime = "1970-01-01T00:00:00.000Z";
-  }
-  console.log("This is the passing Time", passingTime);
+  const passingTime = params.start_time.includes("00:00")
+    ? `1970-01-01T${params.start_time}.000Z`
+    : "1970-01-01T00:00:00.000Z";
 
   const [mics, count] = await prisma.$transaction([
     prisma.mics.findMany({
-      include: {
-        mic_address: true,
-        mic_cost: true,
-        mic_occurrence: true,
-      },
+      include: { mic_address: true, mic_cost: true, mic_occurrence: true },
       where: {
         day: { in: passingDay },
         borough: { in: passingBorough },
@@ -311,6 +109,5 @@ const getMicTimes = async (params: any) => {
 
   return { mics, count };
 };
-// console.log("Service is working ------", getMics);
 
 export { getMics, getMic, getMicTimes, getMicHost };
